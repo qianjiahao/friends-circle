88ce2f464a655b6448422de817ca166e2fa09276
william <1041897388@qq.com>
2015-06-03 15:43:31 +0800

    remove test directive

--- front-end/api/directive/directive.js
+++ front-end/api/directive/directive.js
@@ -5,17 +5,7 @@
 			return {
 				restrict: 'E',
 				template: '<h1>Index Page</h1>',
-				replace: true,
-				link: function (scope, element, attrs) {
-					element.on('click', function () {
-						console.log('click');
-					}).on('mouseenter', function () {
-						console.log('mouseenter');
-					}).on('mouseout', function () {
-						console.log('mouseout');
-					});
-					console.log(angular.element(this));
-				}	
+				replace: true
 			};
 		})
 		.directive('about', function () {
@@ -30,16 +20,6 @@
 				restrict: 'E',
 				template: '<h1>Contact</h1>',
 				replace: true
-			}
-		})
-		.directive('test', function () {
-			return {
-				restrict: 'AE',
-				template: '<input type="text" ng-focus="isFocus">',
-				replace: true,
-				link: function (scope, element, attrs) {
-					console.log(element);;
-				}
 			}
 		})
 		
