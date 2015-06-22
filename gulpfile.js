a6540800279747c9f544595b28a6f87ec1806651
william <1041897388@qq.com>
2015-06-22 11:26:48 +0800

    refactor the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
-var less = require('gulp-less');
-var minify = require('gulp-minify-css');
 var jslint  = require('gulp-jslint');
 var guitl = require('gulp-util');
-var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var livereload = require('gulp-livereload');
 var combine = require('stream-combiner2');
 
-function errorHandle(err) {
+var minifyCSS = require('gulp-minify-css');
+var less = require('gulp-less');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix');
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+
+var cleanCSS = new LessPluginCleanCSS({ advanced: true });
+var autoprefix = new LessPluginAutoPrefix({ browsers: ['last 2 versions'] });
+
+var path = {
+	lessPath: 'front-end/assets/styles/**.less',
+	angularJSPath: 'front-end/api/**.js',
+	viewsPath: 'front-end/views/**.html',
+	
+	cleanPath: 'front-end/dist/**',
+	destLessPath: 'front-end/dist/assets/styles',
+	destAngularJSPath : 'front-end/dist/api'
+}
+
+function error(err) {
 	var colors = guitl.colors;
 	console.log('\n');
 	guitl.log(colors.red('Error'));
@@ -20,56 +39,56 @@ function errorHandle(err) {
 	console.log('\n');
 }
 
+gulp.task('clean', function () {
+	gulp.src(path.cleanPath, {read: false})
+		.pipe(clean())
+});
+
 gulp.task('watch', function () {
 	livereload.listen();
 
-	gulp.watch('front-end/**').on('change', function (file) {
-		livereload.changed(file);
-	});
-	gulp.watch('back-end/**').on('change', function (file) {
-		livereload.changed(file);
-	});
+	gulp.watch(path.lessPath, ['lessCSS']);
 
-});
-
-gulp.task('uglifyJS', function () {
-	var combined = combine.obj([
-		gulp.src('front-end/assets/js/**'),
-		jslint(),
-		uglify(),
-		gulp.dest('front-end/dist/assets/js')
-	]);
-
-	combined.on('error',errorHandle);
+	gulp.watch(path.angularJSPath, ['uglifyAngularJS']);
+		
+	gulp.watch(path.viewsPath)
+		.on('change', function (file) {
+			livereload.changed(file);
+		});
 });
 
 gulp.task('lessCSS', function () {
 	var combined = combine.obj([
-		gulp.src('front-end/assets/styles/**'),
-		less(),
-		minify(),
-		gulp.dest('front-end/dist/assets/styles')
+		gulp.src(path.lessPath),
+		plumber(),
+		sourcemaps.init(),
+		less({
+			plugin: [autoprefix, cleanCSS]
+		}),
+		minifyCSS(),
+		sourcemaps.write('./maps'),
+		gulp.dest(path.destLessPath),
+		livereload()
 	]);
 
-	combined.on('error',errorHandle);
+	combined.on('error',error);
 });
 
 gulp.task('uglifyAngularJS', function () {
 	var combined = combine.obj([
-
-		gulp.src('front-end/api/**.js'),
+		gulp.src(path.angularJSPath),
+		plumber(),
+		sourcemaps.init(),
 		concat('all.js'),
-		gulp.dest('front-end/dist/api'),
+		gulp.dest(path.destAngularJSPath),
 		rename('all.min.js'),
 		uglify(),
-		gulp.dest('front-end/dist/api')
+		sourcemaps.write('./maps'),
+		gulp.dest(path.destAngularJSPath),
+		livereload()
 	]);
 
-	combined.on('error',errorHandle);
+	combined.on('error',error);
 });
 
-gulp.task('default',['watch','uglifyJS','uglifyAngularJS','lessCSS'],function () {
-	gulp.watch('front-end/assets/js/**.js',['uglifyJS']);
-	gulp.watch('front-end/assets/styles/**.less',['lessCSS']);
-	gulp.watch('front-end/api/**.js',['uglifyAngularJS']);
-});
+gulp.task('default',['uglifyAngularJS','lessCSS','watch']);
